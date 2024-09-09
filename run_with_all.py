7b358e10d1969a522092dd1b9bfd40c943e3a585
zhuobinggang <zhuobinggang@gmail.com>
2024-09-09 19:04:23 +0900

    冲冲冲

--- run_with_all.py
+++ run_with_all.py
@@ -1,20 +1,26 @@
-def main():
+def run_with_model_class(model_classes):
     from dataset_verify import final_dataset_need_devset
     from trainer import ModelWrapper, train_and_plot, Rouge_Logger
-    from main import Sector_2024
 
     for fold_index in range(5):
+        # 获取数据集
+        train_set, dev_set, test_set = final_dataset_need_devset(shuffle=True, fold_index=fold_index)
         for repeat_index in range(3):
-            # 获取数据集
-            train_set, dev_set, test_set = final_dataset_need_devset(shuffle=True, fold_index=fold_index)
-            # 初始化模型
-            model_wrapper = ModelWrapper(Sector_2024())
-            model_wrapper.set_meta(fold_index=fold_index, repeat_index=repeat_index)
-            logger = Rouge_Logger(model_wrapper.get_name())  # 添加日志记录器
-            # 训练模型并打印结果
-            train_and_plot(model_wrapper, train_set, dev_set, test_set, batch_size=16, check_step=40, total_step=400, logger=logger)  # 添加logger参数
+            for model_class in model_classes:
+                # 初始化模型
+                model_wrapper = ModelWrapper(model_class())
+                model_wrapper.set_meta(fold_index=fold_index, repeat_index=repeat_index)
+                print(model_wrapper.get_name())
+                logger = Rouge_Logger(model_wrapper.get_name())  # 添加日志记录器
+                # 训练模型并打印结果
+                train_and_plot(model_wrapper, train_set, dev_set, test_set, batch_size=16, check_step=40, total_step=400, logger=logger)  # 添加logger参数
 
-if __name__ == "__main__":  # {{ edit_2 }}
-    main()  # {{ edit_3 }}
+def run_all():
+    from main import Sector_2024
+    from ablation import Sector_without_roberta, Sector_without_crf, Sector_without_title
+    run_with_model_class([Sector_2024, Sector_without_roberta, Sector_without_crf, Sector_without_title])
+
+if __name__ == "__main__":  
+    run_all() 
     import os
     os.system('shutdown')
